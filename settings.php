63c1dbc9c25a56f4b9327d169ffbbc8c3b5028e9
Steve Veerman <steve@veerman.ca>
2015-04-24 18:45:03 -0400

    Cleanup
    Remove old code

--- settings.php
+++ settings.php
@@ -1,19 +1,15 @@
 <?php
 
-define('GTFS_VehiclePositions_url', 'http://opendata.hamilton.ca/GTFS-RT/GTFS_VehiclePositions.pb');
-define('GTFS_TripUpdates_url', 'http://opendata.hamilton.ca/GTFS-RT/GTFS_TripUpdates.pb');
-define('GTFS_ServiceAlerts_url', 'http://opendata.hamilton.ca/GTFS-RT/GTFS_ServiceAlerts.pb');
-
 $dbs = array(
 	'hsrmap_dev' => array('host' => 'localhost', 'user' => 'hsr', 'password' => 'password', 'name' => 'hsrmap')
 );
-$dbs_key = 'hsrmap';
+$dbs_key = 'hsrmap_dev';
 
 $cities = array(
 	'hamilton' => array(
 		'name' => 'Hamilton, Ontario',
-		'lat' => 43.255586, // 43.253793
-		'lng' => -79.873151, // -79.746993
+		'lat' => 43.255586,
+		'lng' => -79.873151,
 		'GTFS_VehiclePositions_url' => 'http://opendata.hamilton.ca/GTFS-RT/GTFS_VehiclePositions.pb',
 		'GTFS_TripUpdates_url' => 'http://opendata.hamilton.ca/GTFS-RT/GTFS_TripUpdates.pb',
 		'GTFS_ServiceAlerts_url' => 'http://opendata.hamilton.ca/GTFS-RT/GTFS_ServiceAlerts.pb'
